5c4fd47a3fcc0ade24c90dace4b2df949dc6c2d4
Yash Gupta <yashguptaalld@gmail.com>
2025-07-02 01:15:13 +0530

    Initial commit

---
+++ lib/core/secrets/app_secrets.dart
@@ -0,0 +1,4 @@
+class AppSecrets {
+  static const supabaseUrl = 'https://wgfltlguvzpamnvnpruh.supabase.co';
+  static const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndnZmx0bGd1dnpwYW1udm5wcnVoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTEzMDU4ODMsImV4cCI6MjA2Njg4MTg4M30.HotfhxT5FmolCaecgZXkfG6qfxo9jpWBotscyM5lbqs';
+}
