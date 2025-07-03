6d26772ad807078062569a30500a01711f4a30cb
Yash Gupta <yashguptaalld@gmail.com>
2025-07-03 17:05:08 +0530

    feat(auth): persist authentication state using Supabase session

--- lib/core/usecase/usecase.dart
+++ lib/core/usecase/usecase.dart
@@ -2,5 +2,7 @@ import 'package:blog_app/core/error/failures.dart';
 import 'package:fpdart/fpdart.dart';
 
 abstract interface class UseCase<SuccessType, Params> {
-  Future<Either<Failure, SuccessType>> call(Params params); 
+  Future<Either<Failure, SuccessType>> call(Params params);
 }
+
+class NoParams {}
--- lib/main.dart
+++ lib/main.dart
@@ -10,19 +10,26 @@ void main() async {
   await initDependencies();
   runApp(
     MultiBlocProvider(
-      providers: [
-        BlocProvider(
-          create:
-              (_) => serviceLocator<AuthBloc>(),
-        ),
-      ],
+      providers: [BlocProvider(create: (_) => serviceLocator<AuthBloc>())],
       child: const MyApp(),
     ),
   );
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    context.read<AuthBloc>().add(AuthIsUserLoggedIn());
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
