2bc3d653e9e5061f97eab35623a7e514c61fc18b
Yash Gupta <yashguptaalld@gmail.com>
2025-07-20 23:11:20 +0530

    feat(blog): calculate reading time using regex and update blog card color styling

--- lib/features/blog/presentation/widgets/blog_card.dart
+++ lib/features/blog/presentation/widgets/blog_card.dart
@@ -1,3 +1,4 @@
+import 'package:blog_app/core/utils/calculate_reading_time.dart';
 import 'package:blog_app/features/blog/domain/entities/blog.dart';
 import 'package:flutter/material.dart';
 
@@ -10,7 +11,7 @@ class BlogCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 200,
-      margin: const EdgeInsets.all(16),
+      margin: const EdgeInsets.all(16).copyWith(bottom: 4),
       padding: const EdgeInsets.all(16),
       decoration: BoxDecoration(
         color: color,
@@ -46,7 +47,7 @@ class BlogCard extends StatelessWidget {
               ),
             ],
           ),
-          const Text('1 min'),
+          Text('${calculateReadingTime(blog.content)} min'),
         ],
       ),
     );
